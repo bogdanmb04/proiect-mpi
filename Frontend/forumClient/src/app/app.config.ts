0341cf838321a6fd74a1a689ae53389cb72886a5
Silviu <183800984+silviu28@users.noreply.github.com>
2026-03-28 12:43:43 +0200

    FIX(CI): remove zone.js

--- Frontend/forumClient/src/app/app.config.ts
+++ Frontend/forumClient/src/app/app.config.ts
@@ -1,12 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideRouter(routes)
   ]
 };
